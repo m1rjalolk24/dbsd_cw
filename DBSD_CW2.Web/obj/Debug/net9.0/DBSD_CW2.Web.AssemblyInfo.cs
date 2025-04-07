2ddd78692a5a5d531dc849baea1a1b0b6ad6ca2a
Mirjalol <mirjalol0111@gmail.com>
2025-04-07 19:35:57 +0500

    Databse inside the project

--- DBSD_CW2.Web/obj/Debug/net9.0/DBSD_CW2.Web.AssemblyInfo.cs
+++ DBSD_CW2.Web/obj/Debug/net9.0/DBSD_CW2.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBSD_CW2.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce991b20e2893b47458bac0688617b2cb440a86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f3e929139b3ab545fd169d8eb47290d9b938c09")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBSD_CW2.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBSD_CW2.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
